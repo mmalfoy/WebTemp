79bf61fbb0d755c30cf55fc4fe8524f8d42475c5
mmalfoy <stpmalfoy@gmail.com>
2023-08-15 19:08:13 +0900

    0815

--- src/MyTravelCreate/MyTravelCreate.js
+++ src/MyTravelCreate/MyTravelCreate.js
@@ -24,29 +24,9 @@ function MyTravelCreate(props) {
             alignItems: 'center', 
             height: '100vh'
         }}>
-            <div className="group490">
-                <h1 className="detailTitle">여행 만들기</h1>
-                <p className="subText">
-                    나의 여행 일정을 만들어 보세요. 장소와 날짜를 직접 설정하고 일일 계획을 구성할 수 있습니다.
-                </p>
-                <div className="rectangle"></div>
-            </div>
-            <div style={{ 
-                display: 'flex', 
-                flexDirection: 'row',
-                justifyContent: 'space-between', 
-                alignItems: 'flex-start',
-                height: '62.6vh',
-                padding: '0 6vw'
-            }}>
-                <div style={{marginRight: '6vw'}}>
-                    <MyTravelMap TravelPlanList={TravelPlanList}/>
-                </div>
-                
                 <div style={{ flexGrow: 1 }}>
                     <MyTravelCreateLists addPlan={addPlan} />
                 </div>
-            </div>
             <button className="addButton" onClick={handleClick}>추가하기</button>
         </div>
     );
